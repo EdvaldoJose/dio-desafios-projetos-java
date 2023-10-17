40453d4010f081f93bd1ea116786655afa966aca
Edvaldo José <edvaldojose003@gmail.com>
2023-10-16 23:26:48 -0300

    feat: Criando variaveis

--- dio-tipos-variaveis/src/BoasVindas.java
+++ dio-tipos-variaveis/src/BoasVindas.java
@@ -1,5 +1,9 @@
 public class BoasVindas {
+
     public static void main(String[] args) {
-        System.out.println("Bem-vindo ao Java todo poderoso!");
+        String meuNome = "Edvaldo";
+        int numero = 5;
+        numero = 10;
+        System.out.println("O numero agora 'e: " + numero + " " + "Meu nome: " + meuNome);
     }
 }
