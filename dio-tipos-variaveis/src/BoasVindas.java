b3d106c0a1419b375d40931d0325c8be38ebf045
Edvaldo José <edvaldojose003@gmail.com>
2023-10-17 18:46:19 -0300

    feat: Commit exercicio1

--- dio-tipos-variaveis/src/BoasVindas.java
+++ dio-tipos-variaveis/src/BoasVindas.java
@@ -3,7 +3,21 @@ public class BoasVindas {
     public static void main(String[] args) {
         String meuNome = "Edvaldo";
         int numero = 5;
-        numero = 10;
-        System.out.println("O numero agora 'e: " + numero + " " + "Meu nome: " + meuNome);
+        // numero = 10;
+        boolean variavel = true;
+        String concatenaNumero = 1 + 1 + 1 + "1";
+        System.out.println(concatenaNumero);
+
+        // numero = numero * -1;
+        // numero = numero + 2;
+        // numero = numero++;
+        System.out.println(numero++);
+        System.out.println(++numero);
+        System.out.println(--numero);
+        variavel = !variavel;
+        System.out.println(variavel);
+
+        System.out.println("O numero agora 'e: " + numero + " " + "Meu nome: " +
+                meuNome);
     }
 }
